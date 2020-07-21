10ce19b7a94f4391a06b4d1ca79f8aa4b47d48c6
Burdeina Tetiana <tania545556@gmail.com>
2020-07-21 22:34:26 +0300

    Create project LocalCoding

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -5,6 +5,7 @@ describe('My Login application', () => {
     it('should login with valid credentials', () => {
         LoginPage.open();
 
+
         LoginPage.login('tomsmith', 'SuperSecretPassword!');
         expect(SecurePage.flashAlert).toBeExisting();
         expect(SecurePage.flashAlert).toHaveTextContaining(
